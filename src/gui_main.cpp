64127d64692bfd83c68fbcacf7796ed12b528831
mseskas@gmail.com <mseskas@gmail.com>
2014-09-14 14:10:03 +0000

    rpi, added one leg control

--- src/gui_main.cpp
+++ src/gui_main.cpp
@@ -52,12 +52,16 @@ void gui_main::build_gui()
     gtk_container_add (GTK_CONTAINER (_window), _fixed_box);
 
 
-pwm_gtk_control * ctr = new pwm_gtk_control("8", 8);
-gtk_fixed_put(GTK_FIXED (_fixed_box), ctr->get_main(), 20, 50);
+pwm_gtk_control * ctr8 = new pwm_gtk_control("8", 8);
+gtk_fixed_put(GTK_FIXED (_fixed_box), ctr8->get_main(), 20, 50);
 
 
-pwm_gtk_control * ctr2 = new pwm_gtk_control("9", 9);
-gtk_fixed_put(GTK_FIXED (_fixed_box), ctr2->get_main(), 400, 50);
+pwm_gtk_control * ctr9 = new pwm_gtk_control("9", 9);
+gtk_fixed_put(GTK_FIXED (_fixed_box), ctr9->get_main(), 400, 50);
+
+
+pwm_gtk_control * ctr10 = new pwm_gtk_control("10", 10);
+gtk_fixed_put(GTK_FIXED (_fixed_box), ctr10->get_main(), 20, 150);
 
     /* Enter the main loop */
     gtk_widget_show_all (_window);
