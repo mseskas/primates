96ce645553bb58a2e103d12519c12df7dc572cef
laptop ubuntu <mseskas@gmail.com>
2015-06-25 20:43:47 +0300

    laptop, clean up

---
+++ include/WalkView.h
@@ -0,0 +1,31 @@
+#ifndef WALKVIEW_H
+#define WALKVIEW_H
+
+#include <gtk/gtk.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "ServoModel.h"
+
+class WalkView
+{
+    public:
+        WalkView(ServoModel * model);
+        GtkWidget * get_main();
+        void build_gui();
+
+        GtkWidget * StandButton;
+        GtkWidget * SitButton;
+        ServoModel * Model;
+
+        static void btnStandClick(GtkWidget *wid, gpointer user_data);
+    private:
+        GtkWidget * fixed;
+
+        GtkWidget * StartButton;
+        GtkWidget * SetParamsButton;
+
+
+
+};
+
+#endif // WALKVIEW_H
---
+++ src/WalkView.cpp
@@ -0,0 +1,46 @@
+#include "WalkView.h"
+
+WalkView::WalkView(ServoModel * model)
+{
+    Model = model;
+    build_gui();
+}
+
+
+GtkWidget * WalkView::get_main()
+{
+    return this->fixed;
+}
+
+void WalkView::btnStandClick(GtkWidget *wid, gpointer user_data)
+{
+    WalkView * obj = (WalkView * )user_data;
+    float angle = obj->Model->ServoPositions[0] + 10;
+    obj->Model->ServoPositions[0] = angle;
+    obj->Model->SetSinglePosition(0, angle);
+}
+
+void WalkView::build_gui(){
+    fixed = gtk_fixed_new();
+    gtk_container_set_border_width(GTK_CONTAINER (fixed), 2);
+
+    StandButton = gtk_button_new_with_label("Stand");
+    g_signal_connect (StandButton, "clicked", G_CALLBACK (WalkView::btnStandClick), this);
+    gtk_widget_set_usize(StandButton, 100, 40);
+    gtk_fixed_put(GTK_FIXED (fixed), StandButton, 150, 20);
+
+    SitButton = gtk_button_new_with_label("Sit");
+    //g_signal_connect (SitButton, "clicked", G_CALLBACK (RewardView::btnStartClick), this);
+    gtk_widget_set_usize(SitButton, 100, 40);
+    gtk_fixed_put(GTK_FIXED (fixed), SitButton, 150, 60);
+
+
+    GtkWidget * scale = gtk_hscale_new_with_range(0, 100, 1);
+    gtk_range_set_value(GTK_RANGE(scale), 0);
+
+    gtk_widget_set_usize(scale, 130, 45);
+   // g_signal_connect (scale, "value-changed", G_CALLBACK (pwm_gtk_control::scale_value_changed), this);
+    gtk_fixed_put(GTK_FIXED (fixed), scale, 10, 0);
+
+
+}
